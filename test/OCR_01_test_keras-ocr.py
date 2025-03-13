16cc8bbba0e5a24b485ac6fd6d48e2a9259981cd
chipmunk-tail <j1997m@naver.com>
2025-03-13 01:34:51 +0000

    fix ocr

--- test/OCR_01_test_keras-ocr.py
+++ test/OCR_01_test_keras-ocr.py
@@ -1,7 +1,7 @@
 
 # OCR model = keras_ocr
 # pip install keras-ocr
-# pip install numpy==1.26.4
+# pip install numpy==1.26.4 keras==2.10 tensorflow==2.10
 
 import keras_ocr
 import matplotlib.pyplot as plt
@@ -10,7 +10,10 @@ import matplotlib.pyplot as plt
 pipeline = keras_ocr.pipeline.Pipeline()
 
 # OCR을 수행할 이미지 경로
-image_path = "./test/test_img/image.png"
+image_name = "exam02"
+image_format = ".png"
+image_folder = "./test/test_img/"
+image_path = image_folder + image_name + image_format
 
 # 이미지 불러오기
 image = keras_ocr.tools.read(image_path)
@@ -35,4 +38,25 @@ plt.savefig('./test/test_img/result.png', dpi=300, bbox_inches='tight')
 plt.close()
 
 
+# subplot 결과 출력
+fig, axes = plt.subplots(1, 2, figsize = (12, 6))
+
+# 오리지널 이미지
+axes[0].imshow(image)
+axes[0].set_title("Original")
+axes[0].axis("off")
+
+# 결과 이미지
+axes[1].imshow(image)
+for text, box in prediction_groups[0]:
+    x_values = [point[0] for point in box]
+    y_values = [point[1] for point in box]
+
+    axes[1].plot(x_values + [x_values[0]] + y_values + [y_values[0]], 'r-')
+    axes[1].text(box[0][0], box[0][1], text, fontsize = 12, color = 'blue', bbox = dict(facecolor = 'white', alpha = 0.6))
+axes[1].set_title("OCR_result")
+axes[1].axis("off")
+
+plt.savefig(image_folder + "keras-ocr_" + image_name + "png", dpi = 300, bbox_inches = 'tight')
+plt.close()
 
---
+++ test/README.md
@@ -0,0 +1,22 @@
+# OCR, 번역 모델 테스트
+
+
+## OCR
+
+### keras-ocr
+keras에서 제공하는 ocr
+
+```pip install tensorflow==2.10 keras-ocr```
+
+tensorflow 2.10 버전까지 작동함
+keras가 2.10 버전인지 확인해야 함
+
+CRAFT를 이용해 언어를 감지하기에 자연스러운 텍스트 감지 가능
+
+CRNN (CNN + RNN) 레이어 사용
+
+Tesseract 보다 더 자연스러운 외곡된 텍스트 감지
+
+
+
+
